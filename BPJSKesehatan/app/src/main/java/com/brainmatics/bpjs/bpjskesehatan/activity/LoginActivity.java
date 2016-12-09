da8dcd9f24b9207b460dc6cba9272d79b4ecff59
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-09 11:19:52 +0700

    menambahkan log message

--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/LoginActivity.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/LoginActivity.java
@@ -2,14 +2,20 @@ package com.brainmatics.bpjs.bpjskesehatan.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.brainmatics.bpjs.bpjskesehatan.R;
 
 public class LoginActivity extends Activity {
 
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // menampilkan tulisan di log
+        Log.d(TAG, "Menjalankan onCreate dalam LoginActivity");
     }
 }
