d13be619b3a208237fb51206bfc1f9d5e85ad43f
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-09 11:15:44 +0700

    membuat activity kosong

---
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.brainmatics.bpjs.bpjskesehatan.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.brainmatics.bpjs.bpjskesehatan.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
