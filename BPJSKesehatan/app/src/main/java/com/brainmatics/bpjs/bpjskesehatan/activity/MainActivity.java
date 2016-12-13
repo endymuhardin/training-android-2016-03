d0e2486cc6984c497d3b176d53dc7ecfdb9103eb
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-13 13:31:26 +0700

    menambahkan blank screen tagihan

--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/MainActivity.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 
 import com.brainmatics.bpjs.bpjskesehatan.R;
 import com.brainmatics.bpjs.bpjskesehatan.fragment.NavigationDrawerFragment;
+import com.brainmatics.bpjs.bpjskesehatan.fragment.TagihanFragment;
 import com.brainmatics.bpjs.bpjskesehatan.fragment.WelcomeFragment;
 
 public class MainActivity extends Activity
@@ -50,10 +51,20 @@ public class MainActivity extends Activity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
+        Fragment fr = null;
+        switch (position) {
+            case 0:
+                fr = new TagihanFragment();
+                break;
+            default:
+                fr = new WelcomeFragment();
+                break;
+        }
+
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, fr)
                 .commit();
     }
 
@@ -76,46 +87,6 @@ public class MainActivity extends Activity
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
 }
