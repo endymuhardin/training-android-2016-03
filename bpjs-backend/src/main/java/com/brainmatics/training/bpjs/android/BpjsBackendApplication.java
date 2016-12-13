b977deec8a2e54db065b15113e246c6ce6406eca
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-13 13:35:08 +0700

    starter project backend

---
+++ bpjs-backend/src/main/java/com/brainmatics/training/bpjs/android/BpjsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.brainmatics.training.bpjs.android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpjsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpjsBackendApplication.class, args);
+	}
+}
