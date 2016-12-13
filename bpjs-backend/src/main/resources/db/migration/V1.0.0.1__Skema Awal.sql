959e918e4eb8c0c506bcc71c9a6f1a42c4fbd0ed
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-13 13:45:14 +0700

    skema db dan data awal

---
+++ bpjs-backend/src/main/resources/db/migration/V1.0.0.1__Skema Awal.sql
@@ -0,0 +1,18 @@
+create table peserta (
+  id varchar(36),
+  nama varchar(255) not null,
+  email varchar(255) not null,
+  tanggal_lahir date not null, 
+  primary key (id), 
+  unique (email)
+);
+
+create table tagihan (
+  id varchar(36),
+  id_peserta varchar(36) not null,
+  tanggal_tagihan date not null,
+  tanggal_jatuh_tempo date not null,
+  nilai number(19,2) not null,
+  primary key (id),
+  foreign key (id_peserta) references peserta(id)
+);
---
+++ bpjs-backend/src/main/resources/db/migration/V1.0.0.2__Data Awal.sql
@@ -0,0 +1,5 @@
+insert into peserta(id, nama, email, tanggal_lahir)
+values ('p001', 'Peserta 001', 'peserta001@example.com', '1945-08-17');
+
+insert into tagihan(id, id_peserta, tanggal_tagihan, tanggal_jatuh_tempo, nilai)
+values ('t001', 'p001', '2016-01-01', '2016-01-20', 150000.00);
