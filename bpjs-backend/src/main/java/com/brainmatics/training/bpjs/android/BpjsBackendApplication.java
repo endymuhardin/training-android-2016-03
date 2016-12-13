a61ed52d6d4fe4036ea1c4a0338ff7d9fed638ba
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-13 13:50:44 +0700

    enable jsr310

--- bpjs-backend/src/main/java/com/brainmatics/training/bpjs/android/BpjsBackendApplication.java
+++ bpjs-backend/src/main/java/com/brainmatics/training/bpjs/android/BpjsBackendApplication.java
@@ -2,7 +2,12 @@ package com.brainmatics.training.bpjs.android;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+@EntityScan(
+    basePackageClasses = {BpjsBackendApplication.class, Jsr310JpaConverters.class}
+)
 @SpringBootApplication
 public class BpjsBackendApplication {
 
