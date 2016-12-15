f0113cce3e6733c25325007ee9afd881b5925060
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-15 16:58:39 +0700

    update db dan tampilkan screen tagihan pada saat terima fcm

--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/LoginActivity.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/activity/LoginActivity.java
@@ -2,19 +2,11 @@ package com.brainmatics.bpjs.bpjskesehatan.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.brainmatics.bpjs.bpjskesehatan.R;
-import com.brainmatics.bpjs.bpjskesehatan.db.BpjsDbHelper;
-import com.brainmatics.bpjs.bpjskesehatan.dto.Page;
-import com.brainmatics.bpjs.bpjskesehatan.dto.Tagihan;
-import com.brainmatics.bpjs.bpjskesehatan.service.BackendService;
-
-import java.util.List;
 
 public class LoginActivity extends Activity {
 
@@ -35,40 +27,6 @@ public class LoginActivity extends Activity {
     }
 
     public void btnRegistrasiClicked(View v){
-
-        new AsyncTask<Void, Void, String>() {
-                    @Override
-                    protected String doInBackground(Void... voids) {
-                        try {
-
-                            BackendService service = new BackendService();
-                            Page<Tagihan> hasil = service.semuaTagihan("p001");
-                            Log.d(TAG, "Jumlah data : "+hasil.getTotalElements());
-                            List<Tagihan> data = hasil.getContent();
-
-                            BpjsDbHelper db = new BpjsDbHelper(LoginActivity.this);
-                            if(!data.isEmpty()){
-                                db.kosongkanTabelTagihan();
-                            }
-
-                            for(Tagihan t : data){
-                                Log.d(TAG, "Tanggal Tagihan : "+t.getTanggalTagihan());
-                                Log.d(TAG, "Nilai Tagihan : "+t.getNilai());
-                                db.insertTagihan(t);
-                            }
-                        } catch (Exception err){
-                            Log.e(TAG, err.getMessage());
-                        }
-
-                        return null;
-                    }
-
-                    @Override
-                    protected void onPostExecute(String s) {
-                        Toast.makeText(LoginActivity.this, "Request Tagihan Selesai",
-                                Toast.LENGTH_LONG).show();
-                    }
-        }.execute();
 
     }
 }
--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/service/TerimaFirebaseMessageService.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/service/TerimaFirebaseMessageService.java
@@ -2,14 +2,25 @@ package com.brainmatics.bpjs.bpjskesehatan.service;
 
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.brainmatics.bpjs.bpjskesehatan.R;
+import com.brainmatics.bpjs.bpjskesehatan.activity.LoginActivity;
+import com.brainmatics.bpjs.bpjskesehatan.activity.MainActivity;
+import com.brainmatics.bpjs.bpjskesehatan.db.BpjsDbHelper;
+import com.brainmatics.bpjs.bpjskesehatan.dto.Page;
+import com.brainmatics.bpjs.bpjskesehatan.dto.Tagihan;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.List;
 import java.util.Map;
 
 public class TerimaFirebaseMessageService extends FirebaseMessagingService {
@@ -23,19 +34,67 @@ public class TerimaFirebaseMessageService extends FirebaseMessagingService {
         Log.d(TAG, "FCM Message Content : "+msg.getData());
 
         Map<String, String> data = msg.getData();
+        String idPeserta = data.get("id");
+        updateDataTagihan(idPeserta);
+    }
 
-        // tampilkan notifikasi
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.logo_only)
-                        .setContentTitle("Tagihan Baru")
-                        .setContentText("Ada tagihan baru untuk peserta "
-                        +data.get("nama"));
+    private void updateDataTagihan(String idPeserta){
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... id) {
+                String nama = null;
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                try {
 
-        int notificationId = 100;
-        mNotificationManager.notify(notificationId, mBuilder.build());
+                    BackendService service = new BackendService();
+                    Page<Tagihan> hasil = service.semuaTagihan(id[0]);
+                    Log.d(TAG, "Jumlah data : "+hasil.getTotalElements());
+                    List<Tagihan> data = hasil.getContent();
+
+                    BpjsDbHelper db = new BpjsDbHelper(TerimaFirebaseMessageService.this);
+                    if(!data.isEmpty()){
+                        db.kosongkanTabelTagihan();
+                    }
+                    for(Tagihan t : data){
+                        Log.d(TAG, "Tanggal Tagihan : "+t.getTanggalTagihan());
+                        Log.d(TAG, "Nilai Tagihan : "+t.getNilai());
+                        db.insertTagihan(t);
+                        nama = t.getPeserta().getNama();
+                    }
+                } catch (Exception err){
+                    Log.e(TAG, err.getMessage());
+                }
+
+                return nama;
+            }
+
+            @Override
+            protected void onPostExecute(String nama) {
+                // activity yang mau ditampilkan pada saat notifikasi diklik
+                Intent screenTagihan = new Intent(TerimaFirebaseMessageService.this, MainActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(TerimaFirebaseMessageService.this);
+                stackBuilder.addParentStack(MainActivity.class);
+                stackBuilder.addNextIntent(screenTagihan);
+                PendingIntent tagihanPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+                // tampilkan notifikasi
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(TerimaFirebaseMessageService.this)
+                                .setSmallIcon(R.drawable.logo_only)
+                                .setContentTitle("Tagihan Baru")
+                                .setContentText("Ada tagihan baru untuk peserta "
+                                        +nama);
+                mBuilder.setContentIntent(tagihanPendingIntent);
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                int notificationId = 100;
+                mNotificationManager.notify(notificationId, mBuilder.build());
+            }
+        }.execute(idPeserta);
     }
 }
