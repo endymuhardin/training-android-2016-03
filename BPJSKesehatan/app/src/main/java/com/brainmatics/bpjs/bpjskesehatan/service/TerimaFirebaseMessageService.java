dbdec10998989c8ebb7ceb5350395040755b9b2a
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-15 16:21:13 +0700

    tampilkan notifikasi pada saat terima FCM

--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/service/TerimaFirebaseMessageService.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/service/TerimaFirebaseMessageService.java
@@ -1,12 +1,16 @@
 package com.brainmatics.bpjs.bpjskesehatan.service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.brainmatics.bpjs.bpjskesehatan.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class TerimaFirebaseMessageService extends FirebaseMessagingService {
 
@@ -17,5 +21,21 @@ public class TerimaFirebaseMessageService extends FirebaseMessagingService {
         Log.d(TAG, "FCM Message From : "+msg.getFrom());
         Log.d(TAG, "FCM Message To : "+msg.getTo());
         Log.d(TAG, "FCM Message Content : "+msg.getData());
+
+        Map<String, String> data = msg.getData();
+
+        // tampilkan notifikasi
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.logo_only)
+                        .setContentTitle("Tagihan Baru")
+                        .setContentText("Ada tagihan baru untuk peserta "
+                        +data.get("nama"));
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int notificationId = 100;
+        mNotificationManager.notify(notificationId, mBuilder.build());
     }
 }
