a5a46e1bfb8c22fab70a429c11fb4c4568e8cf7e
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-16 14:18:58 +0700

    implement lookup koordinat GPS

--- BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/fragment/PesertaFragment.java
+++ BPJSKesehatan/app/src/main/java/com/brainmatics/bpjs/bpjskesehatan/fragment/PesertaFragment.java
@@ -1,19 +1,39 @@
 package com.brainmatics.bpjs.bpjskesehatan.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.brainmatics.bpjs.bpjskesehatan.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PesertaFragment extends Fragment {
+public class PesertaFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
+    private static final String TAG = "PesertaFragment";
+    private static final int REQUEST_GPS_PERMISSION = 100;
+
+    private GoogleApiClient googleApiClient;
+    private Location lokasiSaatIni;
+    private TextView txtLatitude;
+    private TextView txtLongitude;
 
     public PesertaFragment() {
         // Required empty public constructor
@@ -23,8 +43,83 @@ public class PesertaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setupGooglePlayServices();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_peserta, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_peserta, container, false);
+
+        txtLatitude = (TextView) fragmentView.findViewById(R.id.txtLatitude);
+        txtLongitude = (TextView) fragmentView.findViewById(R.id.txtLongitude);
+
+        return fragmentView;
     }
 
+    @Override
+    public void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void setupGooglePlayServices() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_GPS_PERMISSION);
+            Log.d(TAG, "Tidak punya permission untuk mengakses GPS");
+
+            return;
+        }
+
+        lokasiSaatIni = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        Log.d(TAG, "Mendapatkan posisi GPS : "+lokasiSaatIni);
+
+        txtLatitude.setText(String.valueOf(lokasiSaatIni.getLatitude()));
+        txtLongitude.setText(String.valueOf(lokasiSaatIni.getLongitude()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == REQUEST_GPS_PERMISSION){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission GPS diijinkan");
+            } else {
+                Log.d(TAG, "Permission GPS tidak diijinkan, tidak dapat menggunakan location request");
+                txtLatitude.setEnabled(false);
+                txtLongitude.setEnabled(false);
+            }
+        }
+    }
 }
