71650c130706f79b3c85600aa91cdacf05a639b4
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-16 10:53:54 +0700

    push fcm ketika update tagihan

--- bpjs-backend/src/main/java/com/brainmatics/training/bpjs/android/controller/BpjsBackendRestController.java
+++ bpjs-backend/src/main/java/com/brainmatics/training/bpjs/android/controller/BpjsBackendRestController.java
@@ -6,10 +6,14 @@ import com.brainmatics.training.bpjs.android.dao.TagihanDao;
 import com.brainmatics.training.bpjs.android.entity.FcmToken;
 import com.brainmatics.training.bpjs.android.entity.Peserta;
 import com.brainmatics.training.bpjs.android.entity.Tagihan;
+import java.util.HashMap;
+import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class BpjsBackendRestController {
+    
+    private static final String FCM_SERVER = "https://fcm.googleapis.com/fcm/send";
+    private static final String FCM_KEY = "AAAArBbTg8k:APA91bGqebA1TUTSHf5oJsWTMfWz3CuroVLDTvdjWOOeQc8lBd2zCxuTJbWhhv4MSoKEl3zTOxFwBJkiOEHmQFtq0aM6Vpxr7SU6Nv-5BywPPQlmennTzE0a8IvzMOLeDQWK9y3H36xIAVTOYYTiedVdpDjvqDyXOw";
     
     @Autowired private TagihanDao tagihanDao;
     @Autowired private FcmTokenDao fcmTokenDao;
@@ -40,6 +48,31 @@ public class BpjsBackendRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public void insertTagihan(@RequestBody @Valid Tagihan t){
         tagihanDao.save(t);
+        
+        // kirim FCM ke device untuk notifikasi tagihan
+        FcmToken token = fcmTokenDao.findByPeserta(t.getPeserta());
+        if(token == null) {
+            return;
+        }
+        
+        // Data yang mau dikirim
+        Map<String, Object> fcmMsg = new HashMap<>();
+        Map<String, String> fcmData = new HashMap<>();
+        fcmData.put("id", t.getPeserta().getId());
+        fcmData.put("nomor", t.getPeserta().getNomor());        
+        fcmData.put("nama", t.getPeserta().getNama());
+
+        fcmMsg.put("to", token.getFcmToken());
+        fcmMsg.put("data", fcmData);
+        
+        HttpHeaders httpRequestHeader = new HttpHeaders();
+        httpRequestHeader.set("Authorization", "key:"+FCM_KEY);
+        
+        HttpEntity<Map<String, Object>> httpRequestBody = new HttpEntity<>(fcmMsg, httpRequestHeader);
+        
+        RestTemplate httpClient = new RestTemplate();
+        Map<String, Object> response = httpClient.postForObject(FCM_SERVER, httpRequestBody, Map.class);
+        System.out.println("Response : "+response);
     }
     
     @RequestMapping(method = RequestMethod.POST, value = "/token/{peserta}/")
